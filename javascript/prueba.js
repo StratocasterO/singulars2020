c455430bc84402c00bc47b20344230b6366cb8ea
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2020-12-18 14:16:17 +0100

    intro als arrays i objectes, exercicis de funcions i funcions arrow

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -1,3 +1,7 @@
 console.log("Aquest script s'ha carregat des d'un document extern");
 
-document.write(`<p style="font-weight: bold">Això s'ha afegit des de un document .js extern</p>`);
+document.write(`<p>Això s'ha afegit des de un document .js extern</p>`);
+
+function escriure() {
+    document.write(`<p>Això s'ha carregat des de un <i>document.write()</i> dins una funció que s'activa amb un botó`);
+}
