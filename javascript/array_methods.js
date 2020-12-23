35f99259165d3736546bbbaa62f62d816c689b35
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2020-12-23 08:54:14 +0100

    metodes d'arrays, bucle for/of

---
+++ javascript/array_methods.js
@@ -0,0 +1,123 @@
+// Mètodes arrays:
+
+var elMeuPrimerArray = ["Oscar", "Ester", 27.8, "Sergi"];
+
+// sel·lecciono l'element amb index 2
+alumne1 = elMeuPrimerArray[2];
+
+console.log(alumne1);
+
+// mostra tot el contingut separat per comes
+console.log(elMeuPrimerArray.toString());
+
+// JOIN() mostra tot el contingut separat pel separador definit
+console.log(elMeuPrimerArray.join(" - "));
+
+// POP() elimina i retorna l'ultim element de l'array
+var sergi = elMeuPrimerArray.pop()
+
+console.log([sergi, elMeuPrimerArray]);
+
+// PUSH() afegeix al final els elements
+elMeuPrimerArray.push("Sergi", "Dani");
+
+console.log(elMeuPrimerArray);
+
+// SHIFT() elimina i retorna el primer element de l'array
+var oscar = elMeuPrimerArray.shift();
+
+console.log([oscar, elMeuPrimerArray]);
+
+// UNSHIFT() afegeix al principi els elements
+elMeuPrimerArray.unshift("Oscar", "Alex");
+
+console.log(elMeuPrimerArray);
+
+// SPLICE() elimina i retorna 1 element a partir de la posició 3 i afegeix a la mateiax posició els elements
+var eliminados = elMeuPrimerArray.splice(3,1,"Ricardo","Eva","Karina");
+
+console.log([eliminados, elMeuPrimerArray]);
+
+var elMeuSegonArray = ["Bruno", "Pikachu", "Alsik", "Black", "Khaleesi"];
+
+// CONCAT() concatena dos arrays
+console.log(elMeuPrimerArray.concat(elMeuSegonArray));
+
+// SLICE() retorna els elements de l'array des de l'element indicat
+var sliced = elMeuSegonArray.slice(2);
+
+console.log([sliced, elMeuSegonArray]);
+
+console.log("----------------------------------------------");
+
+// -----------------------------------------------------------------------------------------------
+
+// Ordenació Arrays
+
+// SORT() ordena alfabeticament
+elMeuSegonArray.sort();
+
+console.log(elMeuSegonArray);
+
+// REVERSE() inverteix l'ordre dels elements
+elMeuSegonArray.reverse();
+
+console.log(elMeuSegonArray);
+
+// el mètode sort() no funciona amb números
+elMeuTercerArray = [54, 16, 1, -5, 4, 3.14, 4, 1/3];
+
+elMeuTercerArray.sort();
+
+console.log(elMeuTercerArray);
+
+// perquè funcioni li definim una funció anònima (amb funcions arrow) per fer la comparació
+elMeuTercerArray.sort((a,b) => a - b);
+
+console.log(elMeuTercerArray);
+
+console.log("----------------------------------------------");
+
+// --------------------------------------------------------------------------------------------------
+
+// Recòrrer els elements d'un array
+
+var alumnes = "";
+
+for (alumne of elMeuPrimerArray){
+    alumnes += alumne + " ";
+    console.log("Hola, " + alumne);
+}
+
+console.log(alumnes)
+
+console.log("----------------------------------------------");
+
+
+// -----------------------------------------------------------------------------------
+
+// Exercici llista de la compra:
+
+// agafa els productes en un diàleg
+var productes = prompt("Introdueix els productes separats per comes ","");
+
+// separa els productes en elements d'un array
+productes = productes.split(",");
+
+console.log(productes);
+
+// elimina els espais al principi i al final d'un string
+// (uso el for amb un índex perque vull reescriure cada element de l'array)
+for (i = 0; i < productes.length; i++) {
+    productes[i] = productes[i].trim() 
+}
+
+// ordena els prodcutes alfabèticament
+productes.sort();
+
+console.log("Has de comprar:");
+
+// (uso el for/of per pintar els elements a la consola)
+for (producte of productes) {
+    console.log(`  - ${producte}`)
+}
--- javascript/number_methods.js
+++ javascript/number_methods.js
@@ -35,3 +35,54 @@ console.log(smallNum.toExponential(3)); // transforma a notació científica
 // ----------------------------------------------------------------------------------------
 
 console.log(new Date().getTime()); // retorna els milisegons quan han passat des del 1/1/1970
+
+// ----------------------------------------------------------------------------------------
+
+console.log(Math.PI);  // carrega la constant PI de l'objecte predefinit Math
+
+// Números aleatoris:
+
+console.log(Math.random()) // un número aleatori entre 0 i 1
+
+console.log(Math.ceil(Math.random()*20))    // un número aleatori entre 1 i 20 (mètode Omar, que és molt més maco)
+
+console.log(parseInt(Math.random()*21))     // un número aleatori entre 1 i 20 (mètode tradicional)
+
+// ----------------------------------------------------------------------------------------
+
+// Sel·lecció aleatòria d'un dia entre 1900 i 2020:
+
+var cumple = { "dia" : 0,       // objecte amb les propietats dia, mes i any
+               "mes" : 0,
+               "any" : 0
+             };
+
+var trenta = [4, 6, 9, 11];
+
+cumple.any = random(1900,2020); // genera l'any
+
+cumple.mes = random(1,12);     // genera el mes
+
+traspas = (cumple.any % 400 == 0) || (cumple.any % 4 == 0 && cumple.any % 100 != 0)
+
+if (cumple.mes == 2) {
+    if (traspas) {
+        cumple.dia = random(1,29);
+    } else {
+        cumple.dia = random(1,28);
+    }
+} else if (trenta.includes(cumple.mes)) {
+    cumple.dia = random(1,30);
+} else {
+    cumple.dia = random(1,31);
+}
+
+mesos = ["gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost", "setembre", "octubre", "novembre", "desembre"];
+
+console.log(`El teu aniversari és el ${cumple.dia} de ${mesos[cumple.mes - 1]} del ${cumple.any}`);
+
+// funció que retorna un sencer entre min i max, inclosos
+function random(min, max){
+    return min + parseInt(Math.random()*(max - min + 1));
+}
+
