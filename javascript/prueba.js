422326e146a97e93e7b37f31a264c1a1887d898f
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2021-02-09 14:49:24 +0100

    apunts actualitzats, exercicis de fetch, inici de CRUD amb firebase

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -1,3 +1,23 @@
+/*******             LLIBRERIA DE FUNCIONS NOSECUANTOS             *******/
+/*
+
+    * Aquesta llibreria ha estat desenvolupada per nosequi per fer nosequé.
+
+    Les funcions que pots usar son aquestes:
+
+    llibreria.ordenar() -> serveix per ordenar nosequina cosa
+
+    llibreria.resetejar() -> sdkgosfgjhsflgkjsdfgklsdg
+
+    llibreria.arraysGuais(array, mode, color) -> jfaposfjalñfjaslfjaf
+                            !array -> l'array que vols deixar toh guai
+                            ?mode -> el mode de nosecuantos (A, C, B, X, Y)
+                            //color -> un color introduit en hexadecimal amb una string "000000"
+
+    (tot això és un exemple de documentació de codi)
+
+*/
+
 console.log("Aquest script s'ha carregat des d'un document extern");
 
 document.write(`<p>Això s'ha afegit des de un document .js extern</p>`);
